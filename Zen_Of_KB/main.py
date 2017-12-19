5a740cfd34b0bde8c0f2851dd8044625c49a5cd4
IllyaOhorodnyk <illyaohorodnyk@gmail.com>
2017-12-19 12:42:45 +0200

    Update main.py

--- Zen_Of_KB/main.py
+++ Zen_Of_KB/main.py
@@ -1,11 +1,12 @@
 text = """
-The Zen of Cyber Security
+The Zen of Cyber Security.
 
-The KB is better then KNIT
-The Pachomova is better then Matsetska
-The IDLE is better then PyCharm
-The Java is most || powerful || programing language on the world
+The KB is better then KNIT.
+The Pachomova is better then Matsetska.
+The IDLE is better then PyCharm.
+The Java is most || powerful || programing language on the world.
                  \/          \/
+Have. You. Any. Questions?
 """
 
 
