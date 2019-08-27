57ba9c39b9fe34480d23354cd24cc59ebf75b080
Illya Ohorodnyk <illyaohorodnyk@gmail.com>
2019-08-27 15:42:51 +0300

    Edditing WPVulnsCatalog

--- WPVulnsCatalog/main.py
+++ WPVulnsCatalog/main.py
@@ -1,10 +1,27 @@
 import os
 import sys
 import cons_menu
+import json
+import ssl
+import logging
+from urllib.request import Request, urlopen
+
+from parser import Parser
 
 
 def main():
-	print("Hello, world!")
+	config = json.load(open("config.json"))
+	
+	url = "https://" + config["resources"][0]["domain"] + "/" + config["resources"][0]["reign"]
+	headers = {"X-Mashape-Key": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"}
+	gcontext = ssl.SSLContext()
+
+
+	request = Request(url, headers=headers)
+	data = urlopen(request, context=gcontext).read().decode("utf-8")
+
+	parser = Parser()
+	parser.feed(data)
 
 if __name__ == "__main__":
 	main()
